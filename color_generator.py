9af57a8e1d58c71a90ce91d702203f4d12dc2652
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2022-03-11 14:31:02 +0100

    Add Xresources parsing

--- color_generator.py
+++ color_generator.py
@@ -1,19 +1,69 @@
 import json
 import click
 from jinja2 import Environment, PackageLoader, select_autoescape
+from typing import Dict
+from pathlib import Path
 
 # @click.group()
 # def main():
 #     pass
 
 
+def parse_json(path: Path) -> Dict:
+    with open(path, "r") as f:
+        return json.load(f)
+
+
+def parse_xresources(path: Path) -> Dict:
+    with open(path, "r") as f:
+        lines = f.readlines()
+
+    prefixes = ["*."]
+
+    def remove_prefixes(line: str) -> str:
+        for prefix in prefixes:
+            line = line.removeprefix(prefix)
+        return line
+
+    colorscheme = {}
+    colors = {}
+
+    # extract values from file
+    matches = ["foreground", "background", "cursorColor", "color"]
+    for line in lines:
+        if line.startswith("!"):
+            continue
+
+        line = remove_prefixes(line)
+        if any([match in line for match in matches]):
+            name, value = (v.strip() for v in line.split(':'))
+            if 'color' not in name:
+                colorscheme[name] = value
+                continue
+
+            #color{num}
+            index = int(name.removeprefix('color'))
+            colors[index] = value
+
+    # add color list to result dictionary
+    colorscheme['color'] = []
+    for index in sorted(colors.keys()):
+        colorscheme['color'].append(colors[index])
+
+    return colorscheme
+
+
 @click.command()
 @click.argument("colorscheme_path", type=click.Path(exists=True))
 def generate(colorscheme_path: str):
-    TEMPLATE_PATH = "templates/kitty.conf"
+    colorscheme_path = Path(colorscheme_path)
+    extension = colorscheme_path.suffix.lower()
+    if extension == ".xresources":
+        colorscheme = parse_xresources(colorscheme_path)
+    elif extension == ".json":
+        colorscheme = parse_json(colorscheme_path)
 
-    with open(colorscheme_path, "r") as f:
-        colorscheme = json.load(f)
+    TEMPLATE_PATH = "templates/kitty.conf"
 
     foreground = colorscheme["foreground"]
     background = colorscheme["background"]
@@ -24,7 +74,8 @@ def generate(colorscheme_path: str):
         loader=PackageLoader("color_generator"), autoescape=select_autoescape()
     )
     template = env.get_template("kitty.conf")
-    print(template.render(foreground=foreground,background=background,colors=colors)) 
+    print(template.render(foreground=foreground, background=background, colors=colors))
+
 
 if __name__ == "__main__":
     generate()
